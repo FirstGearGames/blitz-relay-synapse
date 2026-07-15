5dae3322032a69f7467330731c05eb796a33b125
abdelfattahradwan <aradwan620@gmail.com>
2026-07-15 17:30:00 +0300

    use `JsonSerializerDefaults.Web` in source generator options

--- src/BlitzRelay/Serialization/AppJsonSerializerContext.cs
+++ src/BlitzRelay/Serialization/AppJsonSerializerContext.cs
@@ -1,11 +1,12 @@
 ﻿using BlitzRelay.Hosting;
 using BlitzRelay.Http;
 using BlitzRelay.Rooms;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace BlitzRelay.Serialization;
 
-[JsonSourceGenerationOptions]
+[JsonSourceGenerationOptions(JsonSerializerDefaults.Web)]
 [JsonSerializable(typeof(CorsConfiguration))]
 [JsonSerializable(typeof(RelayHttpApi.CreateRoomRequest))]
 [JsonSerializable(typeof(RelayHttpApi.PatchRoomRequest))]
